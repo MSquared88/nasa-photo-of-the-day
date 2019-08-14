d3dd1f84b2d939632ed6c9187e7f3049cd23bd91
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-08-14 15:02:30 -0500

    made componetnts and started get request

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "./App.css";
+import Header from './components/Header'
+import Footer from './components/Footer'
+import PhotoGallery from './components/PhotoGallery'
+import Hero from './components/Hero'
+
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <Header/>
+      <Hero/>
+      <div className = 'container'>
+        <PhotoGallery/>
+      </div>
+
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/PhotoGallery.js
@@ -0,0 +1,15 @@
+import React,{useState, useEffect} from 'react';
+import axios from 'axios'
+
+export default function PhotoGallery() {
+
+    const [apodImg, setApodImg] = useState([])
+    useEffect(() => {
+        
+    }, [])
+    return (
+        <div>
+            
+        </div>
+    );
+};   
